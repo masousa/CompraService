9485dbd9f5d22c4bf865f787058abd40e62a4314
Matheus Sousa <alvessousam6@gmail.com>
2023-11-11 14:45:39 -0300

    Adicionando as entidades e as interfaces para cada tabela no banco de dados

---
+++ src/main/java/tech/ada/tenthirty/ecommerce/model/Compra.java
@@ -0,0 +1,31 @@
+package tech.ada.tenthirty.ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "compra")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private LocalDateTime dataCompra;
+
+    private BigDecimal valorTotal;
+
+    private StatusCompra statusCompra;
+
+    private String identificador;
+
+}
