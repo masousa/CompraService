dcb39f53195b0c546905a4f14cd0b488e0ec826f
Matheus Sousa <alvessousam6@gmail.com>
2023-11-13 02:15:05 -0300

    adicionando consultas por identificadores de compra e item

--- src/main/java/tech/ada/tenthirty/ecommerce/repository/ItemRepository.java
+++ src/main/java/tech/ada/tenthirty/ecommerce/repository/ItemRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.ada.tenthirty.ecommerce.model.Item;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByCompraId(Long compraId);
 }
